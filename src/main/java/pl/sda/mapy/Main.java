2f8f7155c8d15263be4b583cdddf5d0464deeb95
Rafal <rostkos@gmail.com>
2020-03-08 10:57:06 +0100

    mapy cd

--- src/main/java/pl/sda/mapy/Main.java
+++ src/main/java/pl/sda/mapy/Main.java
@@ -1,8 +1,17 @@
 package pl.sda.mapy;
 
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Generyka<String> generyka= new Generyka<String>("test");
+
+        System.out.println(generyka.getField());
+
+        Generyka<Integer> generyka1 = new Generyka<Integer>(765765);
+        System.out.println(generyka1.getField());
     }
 }
